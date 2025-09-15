e6985098bf4cbe2dc3a1a3325bef7a0de4c5db82
fykyby <95149002+fykyby@users.noreply.github.com>
2025-09-15 13:30:38 +0200

    Redirect fix & start adding pages

---
+++ src/Controller/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PageController extends AbstractController
+{
+    #[Route('/admin/pages', name: 'app_page')]
+    public function index(): Response
+    {
+        return $this->render('page/index.html.twig', []);
+    }
+}
